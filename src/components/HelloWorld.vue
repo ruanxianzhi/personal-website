7df589e7be88490816b37f844cc8df58d6784ec1
xianzhi.olivia.ruan@ibm.com <xianzhi.olivia.ruan@ibm.com>
2019-11-03 16:43:27 -0500

    use vuetify lib

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-      sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-      diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-      dolor sit amet.
-    </p>
-  </div>
+  <v-toolbar dark>
+    <v-toolbar-title v-html="msg" />
+    <v-spacer />
+
+    <v-btn icon href="https://www.linkedin.com/in/xianzhi-olivia-ruan" right>
+      <v-icon color="grey darken-4">mdi-linkedin</v-icon>
+    </v-btn>
+
+    <v-btn icon href="https://github.com/ruanxianzhi" right>
+      <v-icon color="grey darken-4">mdi-github-circle</v-icon>
+    </v-btn>
+  </v-toolbar>
 </template>
 
 <script lang="ts">
@@ -27,21 +21,3 @@ export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
